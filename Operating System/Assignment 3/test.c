df93cec696dcb93adfdaba1f4da8f89a8494a04f
lohan-shubham <shubhamlohan99@gmail.com>
2020-11-12 17:39:49 +0530

    write-up OS-A3

--- Operating System/Assignment 3/test.c
+++ Operating System/Assignment 3/test.c
@@ -11,29 +11,35 @@
 int main()
 {
 	long pid,tem,i=0;
+	int status;
 	pid=fork();
-	if(pid!=0)
+	if(pid>0)
 	{
 
 		printf("PID: %d\n",getpid());
-		// syscall(441,getpid(),10);
-		struct timeval t1,t2;
-		gettimeofday(&t1,NULL);
-		for(i=0;i<10000000;i++);
-		gettimeofday(&t2,NULL);		
-		printf("\n%lf  this is time 1\n",(double)(t2.tv_usec-t1.tv_usec)/1000 + (double)(t2.tv_sec-t1.tv_sec)*1000);
-		wait(NULL);
-	}
-	else
-	{
 		syscall(441,getpid(),10);
 		struct timeval t1,t2;
 		gettimeofday(&t1,NULL);
 		for(i=0;i<10000000;i++);
+		gettimeofday(&t2,NULL);		
+		printf("\n%lf  this is Parent process\n",(double)(t2.tv_usec-t1.tv_usec)/1000 + (double)(t2.tv_sec-t1.tv_sec)*1000);
+		waitpid(pid,&status,0);
+	}
+	else if(pid==0)
+	{
+		// syscall(441,getpid(),10);
+		struct timeval t1,t2;
+		gettimeofday(&t1,NULL);
+		for(i=0;i<10000000;i++);
 		gettimeofday(&t2,NULL);
-		printf("\n%lf  this is time 2\n",(double)(t2.tv_usec-t1.tv_usec)/1000 + (double)(t2.tv_sec-t1.tv_sec)*1000);
+		printf("\n%lf  this is Child process\n",(double)(t2.tv_usec-t1.tv_usec)/1000 + (double)(t2.tv_sec-t1.tv_sec)*1000);
 		
 
+	}
+	else{
+
+		printf("Can't create child process\n");
+		return -1;
 	}
 	return 0;
 
